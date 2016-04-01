e48b111f06b101d9db53e67de563bc766e2ff7c3
trevorpburke <trevorpburke@gmail.com>
2016-04-01 01:22:09 -0700

    change page to allow input search; modify script to search for location at 'enter' press

--- projects/map_app.js
+++ projects/map_app.js
@@ -2,15 +2,6 @@ L.mapbox.accessToken = 'pk.eyJ1IjoidHJldm9ycGJ1cmtlIiwiYSI6ImNpazA2MzJwMTAwdDV4Y
 var geocoder = L.mapbox.geocoder('mapbox.places'),
     map = L.mapbox.map('map', 'examples.map-h67hf2ic');
 
-var cities = {
-	sf: "San Francisco, CA",
-	nyc: "New York City, NY",
-	london: "London, UK",
-	vegas: "Las Vegas, NV",
-	la: "Los Angeles, CA",
-	newOrleans: "New Orleans, LA"
-};
-
 function showMap(err, data) {
     // The geocoder can return an area, like a city, or a
     // point, like an address. Here we handle both cases,
@@ -23,7 +14,18 @@ function showMap(err, data) {
 };
 
 
-function musicQueueOnChange(){
-	// <select> onchange will queue spotify song 
-	// from song object  
+function musicQueue(){
+	//  TODO <input> will query spotify for song matching input query
+    // need to include modal that informs user that inputted query does not match any songs 
+    // maybe implement mapbox or html5 location request to automate music queue 
 }
+
+function changeMapStyle(){
+    // TODO option to change map style? 
+}
+
+function makePlaylist() {
+    // TODO allow users to create of songs about city 
+}
+
+// OAuth to allow users to save playlists made? 
