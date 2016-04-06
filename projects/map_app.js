4fcd56d4a5bf71ccfbe2f5b9e4b1cb16c977279b
trevorpburke <trevorpburke@gmail.com>
2016-04-05 19:19:12 -0700

    change js code to work in both Firefox and Chrome

--- projects/map_app.js
+++ projects/map_app.js
@@ -2,46 +2,9 @@ L.mapbox.accessToken = 'pk.eyJ1IjoidHJldm9ycGJ1cmtlIiwiYSI6ImNpazA2MzJwMTAwdDV4Y
 var geocoder = L.mapbox.geocoder('mapbox.places'),
     map = L.mapbox.map('map', 'examples.map-h67hf2ic');
 
-document.getElementById("search").addEventListener("keypress",
-                                                    changeMapAndSong);
-
 var spotifyApi = new SpotifyWebApi();
 
-/*var results = function(){
-    if (event.keyCode == "13"){
-        var data=this.value;
-        geocoder.query(this.value, showMap);
-    }
-}
-*/
-
-
-function changeMapAndSong(){
-    if (event.keyCode == "13"){
-        var data=this.value;
-        geocoder.query(this.value, showMap);
-        spotifyApi.searchTracks(this.value)
-            .then(function(data){
-                console.log(data);
-                audioObject = new Audio()
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                var current = audioObject.play();                
-                // TODO figure out way 
-                //to stop song on new keypress
-                // TODO stream top 5 songs
-                // TODO create visual player in top right screen
-                // TODO 
-            }, function(err){
-                console.error(err);
-            })
-    }
-}
-
-
 function showMap(err, data) {
-    // The geocoder can return an area, like a city, or a
-    // point, like an address. Here we handle both cases,
-    // by fitting the map bounds to an area or zooming to a point.
     if (data.lbounds) {
         map.fitBounds(data.lbounds);
     } else if (data.latlng) {
@@ -49,13 +12,37 @@ function showMap(err, data) {
     }
 };
 
-function changeMapStyle(){
+window.onload = function() {
+  document.getElementById("search").focus();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('search').addEventListener('keydown', function(e) {
+    if (e.keyCode == 13) {
+        geocoder.query(this.value, showMap);
+        spotifyApi.searchTracks(this.value)
+            .then(function(data){
+                console.log(data);
+                audioObject = new Audio()
+                audioObject = new Audio(data.tracks.items[0].preview_url);
+                audioObject.play();                
+                // TODO songs should stop on new keydown 
+                // TODO stream top 5 songs
+                // TODO create visual player in top right screen
+                // TODO 
+            }, function(err){
+                console.error(err);
+            })
+    }
+  }, false);
+});
+
+function changeMapStyle(style){
     // TODO option to change map style? 
 }
 
 function makePlaylist() {
-    // TODO allow users to create of songs about city 
+    // TODO allow users to create playlist of songs about city 
 }
 
-
-// OAuth to allow users to save playlists made? 
+// TODO OAuth to allow users to save playlists made? 
