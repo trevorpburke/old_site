9f576219b56397429bcf3458b6fa46a09a496129
trevorpburke <trevorpburke@gmail.com>
2016-04-07 12:18:47 -0700

    change code to ES2015, add album artwork after search

--- projects/map_app.js
+++ projects/map_app.js
@@ -1,8 +1,8 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoidHJldm9ycGJ1cmtlIiwiYSI6ImNpazA2MzJwMTAwdDV4Ym01YjRxdThzODQifQ.TbsNOsG_WuAzfkT1fNcH6A';
-var geocoder = L.mapbox.geocoder('mapbox.places'),
+let geocoder = L.mapbox.geocoder('mapbox.places'),
     map = L.mapbox.map('map', 'examples.map-h67hf2ic');
 
-var spotifyApi = new SpotifyWebApi();
+let spotifyApi = new SpotifyWebApi();
 
 function showMap(err, data) {
     if (data.lbounds) {
@@ -16,20 +16,25 @@ window.onload = function() {
   document.getElementById("search").focus();
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+let searchSong = document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('search').addEventListener('keydown', function(e) {
     if (e.keyCode == 13) {
         geocoder.query(this.value, showMap);
         spotifyApi.searchTracks(this.value)
             .then(function(data){
                 console.log(data);
-                audioObject = new Audio()
                 audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();                
+                audioObject.play();
+                let artist = data.tracks.items[0].artists[0].name;
+                let song = data.tracks.items[0].name;
+                let albumUrl = data.tracks.items[0].album.images[1].url;
+                document.getElementById("artist").innerHTML = artist;
+                document.getElementById("song").innerHTML = song;
+                document.getElementById("album-img").src = albumUrl;
                 // TODO songs should stop on new keydown 
                 // TODO stream top 5 songs
                 // TODO create visual player in top right screen
-                // TODO 
+                // TODO
             }, function(err){
                 console.error(err);
             })
@@ -37,12 +42,13 @@ document.addEventListener('DOMContentLoaded', function() {
   }, false);
 });
 
+
 function changeMapStyle(style){
-    // TODO option to change map style? 
+//     // TODO option to change map style? 
 }
 
 function makePlaylist() {
-    // TODO allow users to create playlist of songs about city 
+    //TODO allow users to create playlist of songs about city 
 }
 
 // TODO OAuth to allow users to save playlists made? 
