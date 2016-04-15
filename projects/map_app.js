616db8594215ea50f9a70b13a934ec44f2a23906
trevorpburke <trevorpburke@gmail.com>
2016-04-14 20:25:55 -0700

    add random city/song feature

--- projects/map_app.js
+++ projects/map_app.js
@@ -5,44 +5,90 @@ const geocoder = L.mapbox.geocoder('mapbox.places'),
     map = L.mapbox.map('map', 'examples.map-h67hf2ic');
 
 const spotifyApi = new SpotifyWebApi();
+const search = document.getElementById('search');
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('search').addEventListener('keydown', mapSearch, false);
-  document.getElementById('search').addEventListener('keydown', songSearch, false);
-	}
+  search.addEventListener('keydown', mapSearch, false);
+  search.addEventListener('keydown', songSearch, false);
+  document.getElementById('random').addEventListener('click', randomCity, false);
+  }
 )
 
 function mapSearch(e){
-	if (e.keyCode === 13){
-		geocoder.query(this.value, showMap);
-	}
-}
-
-function showMap(err, data) {
-    if (data.lbounds) {
+  if (e.keyCode === 13){
+    geocoder.query(this.value, function(err, data){
+      if (data.lbounds) {
         map.fitBounds(data.lbounds);
-    } 
-    if (data.latlng) {
+      } 
+      if (data.latlng) {
         map.setView([data.latlng[0], data.latlng[1]], 11);
-    }
+      }
+    });
+  }
 }
 
 function songSearch(e){
-	if (e.keyCode === 13){
-		spotifyApi.searchTracks(this.value)
-			.then(function(data){
-				const track = data.tracks.items[0];
-				const artist = track.artists[0].name;
-	            const song = track.name;
-	            const albumUrl = track.album.images[1].url;
-	            document.getElementById("album-img").src = albumUrl;
-				document.getElementById('album-img').addEventListener('click', function(){
-					const audioObject = new Audio(track.preview_url);
-					const nowPlaying = document.getElementById('now-playing');
-	               	nowPlaying.innerHTML = 'Song: ' + song + '<br>' + 'Artist: ' + artist;
-	               	audioObject.pause();
-	               	audioObject.play();
-				}, false);
-			})
-	}
+  if (e.keyCode === 13){
+    spotifyApi.searchTracks(this.value)
+      .then(function(data){
+        let track = data.tracks.items[0];
+        let artist = track.artists[0].name;
+        let title = track.name;
+        let albumUrl = track.album.images[1].url;
+        document.getElementById("album-img").src = albumUrl;
+
+        document.getElementById('album-img').addEventListener('click', function(){
+          let song = document.getElementById('song');
+          song.src = track.preview_url;
+          song.play();
+          const nowPlaying = document.getElementById('now-playing');
+                  nowPlaying.innerHTML = `Song: ${title} <br> Artist: ${artist}`;
+          document.getElementById('album-img').addEventListener('click', function(){
+            let song = document.getElementById('song');
+            song.src = '';
+            nowPlaying.innerHTML = '';
+            song.pause();  
+          })
+        });
+      })
+  }
+}
+
+var city_names = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
+
+function randomCity(){
+  const random = _.sample(city_names);
+
+  geocoder.query(random, function(err, data){
+      if (data.lbounds) {
+        map.fitBounds(data.lbounds);
+      } 
+      if (data.latlng) {
+        map.setView([data.latlng[0], data.latlng[1]], 11);
+      }
+    });
+
+  spotifyApi.searchTracks(random)
+    .then(function(data){
+        let track = data.tracks.items[0];
+        let artist = track.artists[0].name;
+        let title = track.name;
+        let albumUrl = track.album.images[1].url;
+        document.getElementById("album-img").src = albumUrl;
+
+        document.getElementById('album-img').addEventListener('click', function(){
+          let song = document.getElementById('song');
+          song.src = track.preview_url;
+          song.play();
+          const nowPlaying = document.getElementById('now-playing');
+                  nowPlaying.innerHTML = `Song: ${title} <br> Artist: ${artist}`;
+
+          document.getElementById('album-img').addEventListener('click', function(){
+            let song = document.getElementById('song');
+            song.src = '';
+            nowPlaying.innerHTML = '';
+            song.pause();  
+          })
+        });
+      })
 }
